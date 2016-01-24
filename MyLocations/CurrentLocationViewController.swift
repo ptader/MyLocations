325792d665ac25e29415d783ca9900f211964a21
Paul Tader <ptader@circletee.com>
2016-01-24 14:04:01 -0600

    Adding CoreLocation and basic actions and better error messages.

--- MyLocations/CurrentLocationViewController.swift
+++ MyLocations/CurrentLocationViewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import CoreLocation
 
-class CurrentLocationViewController: UIViewController {
-
+class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate {
+  let locationManager = CLLocationManager()
+  
   @IBOutlet weak var messageLabel: UILabel!
   @IBOutlet weak var latitudeLabel: UILabel!
   @IBOutlet weak var longitudeLabel: UILabel!
@@ -17,8 +19,39 @@ class CurrentLocationViewController: UIViewController {
   @IBOutlet weak var tagButton: UIButton!
   @IBOutlet weak var getButton: UIButton!
   
+  // Here we set setting for the LocationManager, ask for permission 
+  // tell it we are the delegate, want accuracy to ten meters and then 
+  // actually start the location manager.
+  
   @IBAction func getLocation(){
     
+    let authStatus = CLLocationManager.authorizationStatus()
+    
+    if authStatus == .NotDetermined {
+      locationManager.requestWhenInUseAuthorization()
+      return
+    }
+    
+    if authStatus == .Denied || authStatus == .Restricted {
+      showLocationServicesDeniedAlert()
+      return
+    }
+    
+    locationManager.delegate = self
+    locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+    locationManager.startUpdatingLocation()
+  }
+  
+  func showLocationServicesDeniedAlert() {
+    let alert = UIAlertController(title: "Location Services Disabled",
+      message:
+    "Please enable location services for this app in Settings.",
+      preferredStyle: .Alert)
+    
+    let okAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+    alert.addAction(okAction)
+    
+    presentViewController(alert, animated: true, completion: nil)
   }
   
   override func viewDidLoad() {
@@ -31,6 +64,18 @@ class CurrentLocationViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
-
+  //MARK: - CLLocatioManagerDelegate
+  // delegate methods for location manager
+  
+  func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+    print("didFailWithError, \(error)")
+  }
+  
+  func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    let newLocation = locations.last!
+    print("didUpdateLocations \(newLocation)")
+  }
+  
+  
 }
 
